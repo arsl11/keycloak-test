c640ec6cec8b3b49d0428759561d6436c1513485
arsl11 <arifullin1100@mail.ru>
2023-10-16 17:56:43 +0300

    initial commit

---
+++ auth.ts
@@ -0,0 +1,26 @@
+import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next"
+import type { NextAuthOptions as NextAuthConfig } from "next-auth"
+import { getServerSession } from "next-auth"
+
+import Keycloak from "next-auth/providers/keycloak"
+
+export const config = {
+  providers: [
+    Keycloak({
+      clientId: process.env.KEYCLOAK_CLIENT_ID ?? 'client', 
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET ?? '',
+      issuer: process.env.KEYCLOAK_ISSUER,
+      authorization: {
+        params: {
+          scope: 'openid',
+        }
+      } 
+    }),
+  ],
+} satisfies NextAuthConfig
+
+// Helper function to get session without passing config every time
+// https://next-auth.js.org/configuration/nextjs#getserversession
+export function auth(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
+  return getServerSession(...args, config)
+}
